d09723dd708f0642d8102f8cb4e90193bc276d03
Charlie DiFranco <charlie.difranco2@gmail.com>
2020-04-08 19:34:19 -0500

    completed mvp

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,103 @@
 const express = require('express');
-
+const userDb = require('./userDb')
+const postsDb = require('../posts/postDb')
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.use('/:id', validateUserId)
+
+router.post('/', validateUser, async (req, res) => {
+  try {
+    newUser = await userDb.insert(req.body)
+    res.status(201).json(newUser)
+  }
+  catch {
+    res.status(500).json({ message: "Error adding user to the database."})
+  }
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post('/:id/posts', validatePost, validateUserId, async (req, res) => {
+  postsDb.insert(req.body)
+    .then(post => res.status(201).json(post))
+    .catch(err => res.status(400).json({ message: "Error creating user post."}))
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  userDb
+    .get()
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(500).json({ message: "Could not retrieve users."}))
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  console.log("Running")
+  userDb
+    .getById(req.params.id)
+    .then(user => res.status(200).json(user))
+    .catch(err => res.status(500).json({message: "Could not retrieve user."}))
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get('/:id/posts', async (req, res) => {
+  try {
+    const posts = await userDb.getUserPosts(req.user.id)
+    res.status(200).json(posts)
+  }
+  catch {
+    res.status(500).json({ message: "Error retrieving posts for user."})
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', async (req, res) => {
+  try {
+    await userDb.remove(req.user.id)
+    res.status(204).end()
+  }
+  catch {
+    res.status(500).json({ message: "Could not remove user."})
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', async (req, res) => {
+  console.log(req.user.id)
+  console.log(req.body)
+  try {
+    const count = update(req.user.id, req.body.name)
+    res.status(200).json(count) 
+  } 
+  catch {
+    res.status(500).json({ message: "Could not update user."})
+  }
 });
 
 //custom middleware
 
-function validateUserId(req, res, next) {
-  // do your magic!
+async function validateUserId(req, res, next) {
+  const user = await userDb.getById(req.params.id)
+  if (!user){
+    res.status(400).json({ message: "Invalid user id."})
+  } else {
+    req.user = user
+    next()
+  }
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if (!req.body){
+    res.status(400).json({ message: "Missing user data."})
+  } else if (!req.body.name) {
+    res.status(400).json({ message: "Missing required name field."})
+  } else {
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  if (!req.body){
+    res.status(400).json({ message: "Missing post data."})
+  } else if (!req.body.text){
+    res.status(400).json({ message: "Missing required text field."})
+  } else {
+    next()
+  }
 }
 
 module.exports = router;
